2fc8d51c507a30c323f85523ade1e83d988033cb
andrevier <rangel_andre@outlook.com>
2022-03-15 10:27:01 -0300

    issue #17: Script do matlab para executar todos os casos em todas as pastas, completando a automatização dos ensaios triplos.

---
+++ source/faltas triplas no drive/executarScriptsFaltasTriplas.m
@@ -0,0 +1,69 @@
+% Script para executar todos os scripts dos casos de faltas triplas.
+% Como há muitos casos e eles estão divididos em pastas para melhor
+% organização, prefere-se um código recursivo para que as alterações em 1
+% caso possam ser feitas nos demais. 
+
+close all
+clearvars
+
+casosFalta = {'faltaC123','faltaC124','faltaC125', 'faltaC126',...
+              'faltaC134', 'faltaC135', 'faltaC136',...
+              'faltaC145', 'faltaC146',...
+              'faltaC156', ...
+              'faltaC234','faltaC235','faltaC236',...
+              'faltaC245','faltaC246','faltaC256',...
+              'faltaC345','faltaC346',...
+              'faltaC356',...
+              'faltaC456'};
+
+% Salvar os nomes dos arquivos pois eles serão apagados cada vez que os
+% scripts rodarem.
+save('casosFalta.mat','casosFalta')
+
+% Iteração para percorrer as pastas e rodar todos os scripts nelas.
+numPastas = length(casosFalta);
+for j = 1:numPastas
+    % Recarregar os nomes dos arquivos.
+    load('casosFalta.mat','casosFalta')
+    nomePasta = char(casosFalta(j));
+    fprintf('\nExecutando a pasta %s\n', nomePasta)
+    
+    % Entrar na pasta.
+    cd(nomePasta)
+    arquivos = dir;
+    save('arquivos','arquivos')
+    
+    % Ver quantos arquivos tem na pasta.
+    numArquivos = length(arquivos);
+    
+    % Para cada script:
+    for i = 1:numArquivos
+        nomeScript = arquivos(i).name;
+        
+        % se não tiver final 'm', não roda.
+        if isempty(regexp(nomeScript(end),'m')) ~= 1
+            fprintf(' -> %s \n',nomeScript)
+            run(nomeScript)
+            % Espaço para testar se há problemas no valor da CM.
+            if isempty(CM) == 1
+                fprintf('   *VCM do ensaio é vazio\n')
+            end
+            
+            % Recarregar os nomes dos scripts pois eles são apagados pelo 
+            % script que foi rodado.
+            load('arquivos','arquivos')
+        end
+    end
+    
+    % Após executar todos os arquivos, deletar a lista de nomes dos scripts
+    % que foi salva.
+    delete('arquivos.mat')
+    fprintf('...............................\n')
+    
+    % Retornar para a pasta raíz.
+    cd ..
+end
+
+% Apagar a lista com os nomes das pastas
+delete('casosFalta.mat')
+f = msgbox('FIM!');
