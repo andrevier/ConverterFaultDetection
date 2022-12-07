0e62f7e0706843c9c43dade388713754db07d12f
andrevier <rangel_andre@outlook.com>
2022-12-07 07:26:35 -0300

    Issue #13: move the simulation data to an external drive. The simulation data have to be moved to an external drive because some of the .csv files surpass the maximum limit of a Github repository. To do: place the data in a cloud service.

--- source/Simulations/model15ScalarControl/openCircuit/T2FullSpeed/saveData.m
+++
@@ -1,8 +0,0 @@
-% Save the data in .csv file.
-csvwrite('isa.csv',isa);
-csvwrite('isb.csv',isb);
-csvwrite('isc.csv',isc);
-csvwrite('Te.csv',Te);
-csvwrite('Vab.csv',Vab);
-csvwrite('wm.csv',wm);
-csvwrite('time.csv',time);
--- source/Simulations/model15ScalarControl/shortCircuit/T1FullSpeed/__init__.py
+++
@@ -1,1 +0,0 @@
-from .var import Isa, Isb, Isc, Te, time, Vab, wm
