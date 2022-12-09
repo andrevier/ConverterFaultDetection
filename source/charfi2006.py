97d6088a4a67e0ad19d51c962bd2efe37d4fe4e1
andrevier <rangel_andre@outlook.com>
2022-12-09 03:31:30 -0300

    Issue #14: Add the script charfi2006.py. The script charfi2006.py analyze the data according to the paper.

---
+++ source/charfi2006.py
@@ -0,0 +1,9 @@
+"""Implement the paper: Fault Diagnostic in Power System Using Wavelet
+Transforms and Neural Networks.
+Authors: F.Charfi, F.Sellami, and K.Al-Haddad
+Year: 2006
+1. Select the data of the test and the training stage.
+2. Extract the level 6 DWT from windows of the signal.
+3. Train a NN with the pairs using the tensorflow.
+
+"""
